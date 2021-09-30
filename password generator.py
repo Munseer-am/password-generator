371f4c0ce460644e8a7aacbf4e3e7029ae9aaca9
Munseer.am <82161602+Anonymous1829@users.noreply.github.com>
2021-09-30 17:59:02 +0530

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -3,13 +3,13 @@ import random
 import array 
 
  
-MAX_LEN = 30
+max_len = 30
 
 
 
-DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']   
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']   
 
-LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',  
+locase_characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',  
 
                      'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 
 
@@ -19,7 +19,7 @@ LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 
   
 
-UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',  
+upcase_characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',  
 
                      'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q', 
 
@@ -29,31 +29,31 @@ UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 
   
 
-SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',  
+symbols = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',  
 
            '*', '(', ')', '<'] 
 
   
 
 
-COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS 
+COMBINED_LIST = digits + upcase_characters + locase_characters + symbols 
 
   
 
 
-rand_digit = random.choice(DIGITS) 
+rand_digit = random.choice(digits) 
 
-rand_upper = random.choice(UPCASE_CHARACTERS) 
+rand_upper = random.choice(upcase_characters) 
 
-rand_lower = random.choice(LOCASE_CHARACTERS) 
+rand_lower = random.choice(locase_characters) 
 
-rand_symbol = random.choice(SYMBOLS) 
+rand_symbol = random.choice(sysmbols) 
 
   
 
 temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol 
 
-for x in range(MAX_LEN - 4): 
+for x in range(max_len - 4): 
 
     temp_pass = temp_pass + random.choice(COMBINED_LIST) 
 
